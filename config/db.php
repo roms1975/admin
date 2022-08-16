bbc62b45543b483ea265a6c1dde64de6d851db3a
Roman <odggbo@gmail.com>
2022-08-16 17:53:14 +0300

    fix install script

--- config/db.php
+++ config/db.php
@@ -1,14 +1,10 @@
 <?php
 
 return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=romscms',
-    'username' => 'roms',
-    'password' => '9610015',
-    'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+	'class' => 'yii\db\Connection',
+	'dsn' => 'mysql:host=localhost;dbname=romscms',
+	'username' => 'roms',
+	'password' => '9610015',
+	'charset' => 'utf8',
 ];
+
--- src/Installer.php
+++ src/Installer.php
@@ -1,17 +1,51 @@
 <?php 
 
-  namespace roms;
-  
-//  require_once(__DIR__ . "/../vendor/autoload.php");
+	namespace roms;
 
-  class Installer
-  {
-    static public function install() 
-    {
-		echo __DIR__ . "\n";
-		file_put_contents(__DIR__ . "/install.log", "Relize 1.0.4");
-    }
-  
-  }
+//    require_once(__DIR__ . "/../vendor/autoload.php");
+
+class Installer
+{
+	static public function install() 
+	{
+		$config = __DIR__ . "/../config/db.php";
+
+		if (file_exists($config)) {
+			echo "File " . $config . " exist. Please rename it and start again.\n";
+			exit();
+		}
+
+		echo "Enter DB name (or type n to skip): ";
+		$db = trim(fgets(STDIN));
+
+		if ($db == 'n') {
+			echo "Nothing to do. By\n";
+			exit();
+		}
+
+		echo "Enter DB login: ";
+		$user = trim(fgets(STDIN));
+
+		echo "Enter DB password: ";
+		$pass = trim(fgets(STDIN));
+
+		$str = "<?php\n\n" .
+				"return [\n" .
+				"\t'class' => 'yii\db\Connection',\n" .
+				"\t'dsn' => 'mysql:host=localhost;dbname=" . $db . "',\n" .
+				"\t'username' => '" . $user . "',\n" .
+				"\t'password' => '" . $pass . "',\n" .
+				"\t'charset' => 'utf8',\n" .
+			"];\n\n";
+		
+		if (!file_put_contents($config, $str)) {
+			echo "Unable create config file " . $config . "\n";
+			exit();
+		}
+
+		echo "Done\n";
+
+	}
+}
 
 ?>
