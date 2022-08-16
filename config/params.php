9eff24753181d28898d19e2c5e2d319b49a48731
Roman <odggbo@gmail.com>
2022-08-16 12:19:41 +0300

    first commit

---
+++ config/params.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'adminEmail' => 'odggbo@gmail.com',
+    'senderEmail' => 'odggbo@gmail.com',
+    'senderName' => 'Romscms mailer',
+    'main_domen' => '192.168.1.123',
+//    'catalog_root_id' => 5994,
+    'catalog_root_id' => 5492,
+	'struct_perpage' => 12,
+];
